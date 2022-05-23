618ec58aa5c512673d97afdf85621780c79407d7
IlPesceDeveloper <alerapi251001@gmail.com>
2022-05-23 13:28:45 +0200

    3 files modified

--- Quiz/Skyrim/skyrim.js
+++ Quiz/Skyrim/skyrim.js
@@ -1,19 +1,48 @@
-function bulbon(){
-  document.getElementById("logoHint").src="https://i.ibb.co/F3872h2/light-bulb.png";
+function bulbon() {
+  document.getElementById('logoHint').src =
+    'https://i.ibb.co/F3872h2/light-bulb.png';
 }
 
-function bulboff(){
-  document.getElementById("logoHint").src="https://i.ibb.co/NxWkCW6/light-bulb-1.png";
+function bulboff() {
+  document.getElementById('logoHint').src =
+    'https://i.ibb.co/xDFpK98/light-bulb-1.png';
 }
 
-function ics(f){
-  if(f==0){
-    document.getElementById("btnIcs").src="https://i.ibb.co/jG9LfXS/close.png";
-  }else if(f==1){
-    document.getElementById("btnIcs").src="https://i.ibb.co/7CKsbrf/close.png";
+function ics(f) {
+  if (f == 0) {
+    document.getElementById('btnIcs').src =
+      'https://i.ibb.co/jG9LfXS/close.png';
+  } else if (f == 1) {
+    document.getElementById('btnIcs').src =
+      'https://i.ibb.co/7CKsbrf/close.png';
   }
 }
 
-function removeOverlay(){
-  document.getElementById("overlay").style.visibility="collapse";
+let cookie;
+
+function overlaycheck() {
+  cookie = leggiCookie('overlay');
+  if (cookie == 'no') {
+    document.getElementById('overlay').style.visibility = 'collapse';
+  }
+}
+
+function leggiCookie(nomeCookie) {
+  if (document.cookie.length > 0) {
+    var inizio = document.cookie.indexOf(nomeCookie + '=');
+    if (inizio != -1) {
+      inizio = inizio + nomeCookie.length + 1;
+      var fine = document.cookie.indexOf(';', inizio);
+      if (fine == -1) fine = document.cookie.length;
+      return unescape(document.cookie.substring(inizio, fine));
+    } else {
+      return '';
+    }
+  }
+  return '';
+}
+
+function removeOverlay() {
+  document.cookie = "overlay=no";
+  document.getElementById('overlay').style.visibility = 'collapse';
 }
